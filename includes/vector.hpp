1e1fc2734c8fe99765e3f5a763c1e497f283a61a
Oumar Mimouni <oumar.mimouni@gmail.com>
2022-07-24 14:13:32 +0100

    update

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -1,22 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omimouni <omimouni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/24 12:22:13 by omimouni          #+#    #+#             */
-/*   Updated: 2022/07/24 14:09:43 by omimouni         ###   ########.fr       */
+/*   Created: 2022/07/24 14:11:30 by omimouni          #+#    #+#             */
+/*   Updated: 2022/07/24 14:11:57 by omimouni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <vector>
-#include <memory>
 
-class vector2
+
+#ifndef __VECTOR_HPP__
+#define __VECTOR_HPP__
+
+# include <iostream>
+# include <memory>
+# include <vector>
+
+class vector
 {
-
 
 public:
   // Types -------------------
@@ -24,38 +28,45 @@ public:
   typedef std::allocator<int> allocator;
 
   typedef size_t size_type;
-  
+
   //
-  vector2() : _size(0), _capacity(100)
+  vector() : _size(0), _capacity(100)
   {
     _list = alloc.allocate(_capacity);
   }
-  ~vector2() {
+  ~vector()
+  {
     alloc.deallocate(_list, _capacity);
   }
 
   // Iterators -------------
-  iterator begin() {
+  iterator begin()
+  {
     return iterator(&_list[0]);
   }
-  iterator end() {
+  iterator end()
+  {
     return iterator(&_list[_size]);
   }
 
   // Capacity --------------
-  size_type size() const {
+  size_type size() const
+  {
     return _size;
   }
-  size_type capacity() const {
+  size_type capacity() const
+  {
     return _capacity;
   }
 
   // Access Helpers --------
-  int at(size_type n) {
+  int at(size_type n)
+  {
     return _list[n];
   }
 
-  void push_back(int t) {
+  void push_back(int t)
+  {
     _list[_size++] = t;
   }
 
@@ -68,21 +79,4 @@ private:
   allocator alloc;
 };
 
-int main(int argc, char const *argv[])
-{
-  vector2 v;
-
-  v.push_back(10);
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(50);
-
-  vector2::iterator it;
-
-  for (it = v.begin(); it != v.end(); it++)
-  {
-    std::cout << *it << std::endl;
-  }
-  
-  return 0;
-}
+#endif
